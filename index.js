92f261a947c59c37b42f1bd004a7f57de5c5193f
programmer5000-com <programmer5000@programmer5000.com>
2018-06-13 07:58:32 -0400

    Fix linter errors

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 const fetch = require("node-fetch");
-const readline = require("readline");
 const ansi = require("ansi.js");
 const cursor = ansi(process.stdout);
 
@@ -42,35 +41,35 @@ const getStatus = () => {
 	let queued = 0;
 	let started = 0;
 	let finished = 0;
-	entries.forEach(([file, status]) => {
+	entries.forEach(([, status]) => {
 		switch (status) {
-			case 0:
-				queued ++;
-				break;
-			case 1:
-				started ++;
-				break;
-			case 3:
-				finished ++;
+		case 0:
+			queued ++;
+			break;
+		case 1:
+			started ++;
+			break;
+		case 3:
+			finished ++;
 		}
 	});
 	return {queued, started, finished};
 };
 
 const clearAndLog = (...args) => {
-		cursor.moveToColumn(0).eraseLine();
-		console.log(...args);
-		updateBar();
+	cursor.moveToColumn(0).eraseLine();
+	console.log(...args);
+	updateBar();
 };
 
 const updateBar = () => {
 	cursor.fg.reset();
 	cursor.bg.reset();
 	cursor.font.resetBold().resetItalic().resetUnderline().resetInverse();
-	let {queued, started, finished} = getStatus();
+	let {finished} = getStatus();
 	const columns = process.stdout.columns - 2;
 	cursor.moveToColumn(0).eraseLine().
-  write("[" + "█".repeat(Math.floor(finished / tree.length * columns)) + "-".repeat(Math.ceil((tree.length - finished) / tree.length * columns)) + "]");
+		write("[" + "█".repeat(Math.floor(finished / tree.length * columns)) + "-".repeat(Math.ceil((tree.length - finished) / tree.length * columns)) + "]");
 };
 
 const log = (...args) => {
@@ -102,8 +101,8 @@ tree.forEach((file, inc) => {
 		updateBar();
 		if(bypass){
 			await sleep(1000);
-			return queue[file] = 3;
-			updateBar();
+			queue[file] = 3;
+			return updateBar();
 		}
 
 		const procResponse = async function(response, file, retry){
@@ -154,12 +153,14 @@ tree.forEach((file, inc) => {
 					});
 					verbose("start finished sending");
 					const data = await procResponse(resp, null, startIt);
-					return JSON.parse(data).session_id
+					return JSON.parse(data).session_id;
 				}catch(e){
 					procError(e);
 					return await startIt(500, stream);
 				}
 			};
+
+			let sessionId;
 
 			while(hasChunkLeft){
 				const thisRange = [lastByte + 1, Math.min(lastByte + 1 + fileSplitSize, thisSize)];
@@ -186,19 +187,19 @@ tree.forEach((file, inc) => {
 							try{
 								await sleep(delay);
 								const response = await fetch("https://content.dropboxapi.com/2/files/upload_session/append_v2", {
-								  body: stream,
-								  headers: {
-								    Authorization: "Bearer " + token,
-								    "Content-Type": "application/octet-stream",
-								    "Dropbox-Api-Arg": JSON.stringify({
+									body: stream,
+									headers: {
+										Authorization: "Bearer " + token,
+										"Content-Type": "application/octet-stream",
+										"Dropbox-Api-Arg": JSON.stringify({
 											cursor: {
 												offset: thisRange[0],
 												session_id: sessionId
 											},
 											close: false
 										})
-								  },
-								  method: "POST"
+									},
+									method: "POST"
 								});
 								verbose("chunk finished sending");
 								procResponse(response, null, sendThis);
@@ -227,9 +228,9 @@ tree.forEach((file, inc) => {
 							"Content-Type": "application/octet-stream",
 							"Dropbox-Api-Arg": JSON.stringify({
 								cursor: {
-						        "session_id": sessionId,
-						        "offset": lastByte + 1
-						    },
+									"session_id": sessionId,
+									"offset": lastByte + 1
+								},
 								commit: {
 									path: "/" + file,
 									mode: "add",
@@ -254,13 +255,13 @@ tree.forEach((file, inc) => {
 		}else{
 			try{
 				const response = await fetch("https://content.dropboxapi.com/2/files/upload", {
-				  body: fs.createReadStream(path.join(dir, file)),
-				  headers: {
-				    Authorization: "Bearer " + token,
-				    "Content-Type": "application/octet-stream",
-				    "Dropbox-Api-Arg": JSON.stringify({"path": "/" + file, "mode": "add","autorename": true,"mute": false})
-				  },
-				  method: "POST"
+					body: fs.createReadStream(path.join(dir, file)),
+					headers: {
+						Authorization: "Bearer " + token,
+						"Content-Type": "application/octet-stream",
+						"Dropbox-Api-Arg": JSON.stringify({"path": "/" + file, "mode": "add","autorename": true,"mute": false})
+					},
+					method: "POST"
 				});
 				return await procResponse(response, file, fetchIt);
 			}catch(e){
@@ -295,19 +296,19 @@ if(process.stdout.isTTY){
 	process.stdin.resume();
 	process.stdin.on("data", (key) => {
 		key = key.toString();
+		let {queued, started, finished} = getStatus();
 		switch(key){
-		 	case "\u0003":
-				clearAndLog("bye!");
-				process.exit();
-				break;
-			case "s":
-				let {queued, started, finished} = getStatus();
-				cursor.moveToColumn(0).eraseLine();
-				cursor.fg.green();
-				clearAndLog(`${tree.length} total, ${queued} queued, ${started} started, ${finished} finished`);
-				break;
-			default:
-				clearAndLog("unknown command");
-    }
+		case "\u0003":
+			clearAndLog("bye!");
+			process.exit();
+			break;
+		case "s":
+			cursor.moveToColumn(0).eraseLine();
+			cursor.fg.green();
+			clearAndLog(`${tree.length} total, ${queued} queued, ${started} started, ${finished} finished`);
+			break;
+		default:
+			clearAndLog("unknown command");
+		}
 	});
 }
